7f446922ed27c3fbe4036062d76134d1bdd3ee5e
kazukichi <zexyl_x@icloud.com>
2020-08-31 20:24:27 +0900

    ユーザー関係のmodel作成(BaseUserManagerクラス及びUserクラスをカスタマイズするためオーバーライド)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.conf import settings
 
-# Create your models here.
+
+# EmailとPasswordを使用するためBaseUserManagerをオーバーライドしカスタマイズしたBaseUserManagerクラス
+class BaseUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("email is must")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    # Email認証に変更する場合はsuper_userを作成しておいた方が便利
+    def create_super_user(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+# ユーザークラスをオーバーライドしてカスタマイズ
+class User(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
